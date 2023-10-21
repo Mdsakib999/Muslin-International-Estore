3b710a9055fa4e918127a73be4899f0fe937f63d
Mdsakib999 <sakibabrar999@gmail.com>
2023-10-21 19:07:41 +0600

    create project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-red-700'>Vite + React ⭐</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
